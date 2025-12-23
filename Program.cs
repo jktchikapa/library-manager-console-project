42a90a58897c1841925d5492b8d71d0cdb9b799d
jktchikapa <jktchikapa@gmail.com>
2025-12-23 15:55:22 -0500

    Initial commit - Library Manager

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace library_manager_console_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
